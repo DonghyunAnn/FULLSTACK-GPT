772912162c8d481cdb9f8638aced49d312207b7e
DonghyunAnn <danniel1025@gmail.com>
2024-01-19 00:08:24 +0900

    quiz gpt 시작

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,2 +1,39 @@
 import streamlit as st
+from langchain.retrievers import WikipediaRetriever
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders import UnstructuredFileLoader
+
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n", chunk_size=600, chunk_overlap=100
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+st.set_page_config(page_title="Quiz GPT", page_icon="❓")
+
 st.title("Quiz GPT")
+
+with st.sidebar:
+    choice = st.selectbox("Choose what you want to use", ("File", "Wekipedia Article"))
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload .docx, .txt, or .pdf file", type=["pdf", "docx", "txt"]
+        )
+        if file:
+            docs = split_file(file)
+
+    else:
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=5)
+            with st.status("Searching Wikipedia..."):
+                docs = retriever.get_relevant_documents(topic)
