63b4e9a7fa1105d5a93b46d06fca934a0a8f648b
DonghyunAnn <danniel1025@gmail.com>
2024-02-11 23:11:40 +0900

    Site GPT 일차 마무리

--- Home.py
+++ Home.py
@@ -15,10 +15,9 @@ Here are the apps I made:
             
 - [x] [📃 DocumentGPT](/DocumentGPT)
 - [x] [🔒 PrivateGPT](/PrivateGPT)
-- [ ] [❓ QuizGPT](/QuizGPT)
+- [x] [❓ QuizGPT](/QuizGPT)
 - [ ] [🖥️ SiteGPT](/SiteGPT)
 - [ ] [💼 MeetingGPT](/MeetingGPT)
 - [ ] [📈 InvestorGPT](/InvestorGPT)
-
-"""
+    """
 )
